455bd1847d43d984799cbc4b6f5c414c9e62012a
Rouz Alamri <rwzalmry37@gmail.com>
2026-02-28 03:21:25 +0300

    SquareExam

---
+++ squarExam/src/squarexam/SquarExam.java
@@ -0,0 +1,19 @@
+
+package squarexam;
+import java.util.Scanner;
+public class SquarExam {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter the length of one side only ");
+        double x = input.nextDouble();
+        //System.out.println( "the area of the squer is " + (x*x));
+        System.out.print("the area of the squer is ");
+        System.out.println(x*x);
+        
+        
+        
+    }
+    
+}
+ 
