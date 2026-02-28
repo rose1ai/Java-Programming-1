be7feb44be8506449258a12cc34faac2369699a1
Rouz Alamri <rwzalmry37@gmail.com>
2026-02-28 03:18:19 +0300

    lab32

---
+++ lab32/src/lab32/Lab32.java
@@ -0,0 +1,35 @@
+
+package lab32;
+
+import java.util.Scanner;
+
+public class Lab32 {
+
+    
+    public static void main(String[] args) {
+    /*  Scanner input = new Scanner(System.in);
+      int LagestNumber;
+      System.out.println("enter the first integer ");
+      int a = input.nextInt();
+      System.out.println("enter the second integer ");
+      int b = input.nextInt();
+      System.out.println("enter the third integer ");
+      int c = input.nextInt();
+      // انا كنت احسب انه بعد اف لازم احط قوس برايس كبير من بعد اف الى نهاية بعد الس 
+      if (a > b && a>c ){
+          LagestNumber = a; }
+      else if ( b> c ){
+              LagestNumber = b; }
+      else { LagestNumber = c; } 
+              
+      System.out.println(" the largast number is "+ LagestNumber);
+      }
+      */
+      
+      
+      
+      
+        
+    }
+    
+}
