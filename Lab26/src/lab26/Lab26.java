df3c9c6d03b3e70fae2984f2e8c38d46b86663bb
Rouz Alamri <rwzalmry37@gmail.com>
2026-02-28 03:16:52 +0300

    lab26

---
+++ Lab26/src/lab26/Lab26.java
@@ -0,0 +1,21 @@
+
+package lab26;
+
+import java.util.Scanner;
+public class Lab26 {
+
+   
+    public static void main(String[] args) {
+       
+        System.out.println(" eneter number of seconds ");
+        Scanner input = new Scanner(System.in);
+        
+       
+        
+        
+        
+        
+        
+    }
+    
+}
