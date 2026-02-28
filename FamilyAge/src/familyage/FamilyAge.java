f3e555fc7b61745418441bdbb77dbe22de7c2f42
Rouz Alamri <rwzalmry37@gmail.com>
2026-02-28 03:09:57 +0300

    family age

---
+++ FamilyAge/src/familyage/FamilyAge.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package familyage;
+
+/**
+ *
+ * @author rosea
+ */
+public class FamilyAge {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(" my sibling are zeyad and he's 10 tears old, omar and he's 11 and turkey and he's 30 years old ");
+        System.out.println(10+11+30);
+    }
+    
+}
