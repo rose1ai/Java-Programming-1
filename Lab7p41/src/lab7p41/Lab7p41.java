3c4f68e1403f03b3647105a47a713041236a4098
Rouz Alamri <rwzalmry37@gmail.com>
2026-02-28 03:14:13 +0300

    lab7_updated2

---
+++ Lab7p41/src/lab7p41/Lab7p41.java
@@ -0,0 +1,16 @@
+
+package lab7p41;
+
+public class Lab7p41 {
+
+    public static void main(String[] args) {
+       
+        int number = 16 ;
+       
+       }
+        
+        
+                
+    }
+    
+}
